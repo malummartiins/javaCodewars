db4c9aae8b520e98d081ef99404d5363c426f893
Malú <130713333+malummartiins@users.noreply.github.com>
2023-07-20 12:00:54 -0300

    Update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # javaCodewars
- Exercicios em Java que uso para praticar no codewars
+ Exercicios em Java que uso para praticar no Codewars
+comecei a realizar os exercícios em julho/23, todos que conseguir resolver vou hospedar aqui.
--- SpinWords.java
+++ SpinWords.java
@@ -1,3 +1,13 @@
+/*Write a function that takes in a string of one or more words, and returns the same string, but 
+with all five or more letter words reversed (Just like the name of this Kata). Strings passed in willconsist of only 
+letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples:
+
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+spinWords( "This is a test") => returns "This is a test" 
+spinWords( "This is another test" )=> returns "This is rehtona test"*/
+
 public class SpinWords {
 
   public String spinWords(String sentence) {
@@ -15,12 +25,3 @@ public class SpinWords {
   }
 }
 
-/* Write a function that takes in a string of one or more words, and returns the same string, but 
-with all five or more letter words reversed (Just like the name of this Kata). Strings passed in willconsist of only 
-letters and spaces. Spaces will be included only when more than one word is present.
-
-Examples:
-
-spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
-spinWords( "This is a test") => returns "This is a test" 
-spinWords( "This is another test" )=> returns "This is rehtona test"*/
